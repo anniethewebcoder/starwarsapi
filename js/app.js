36893f9ad3e69581b2c8c13905f04b9750f13953
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-04-17 23:31:57 -0500

    getting results function

--- js/app.js
+++ js/app.js
@@ -1,59 +1,40 @@
-// //URL
-// const sw = 'https://swapi.dev/api/';
-
-// //GET PEOPLE
-// async function getPeople() {
-//     const person = sw + 'people/1/'
-//     try {
-//         let res = await fetch(person);
-//         return await res.json();
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// async function renderPeople() {
-//     let people = await getPeople();
-//     console.log(people)
-
-//     let container = document.querySelector('.container');
-//     container.innerHTML = people.name;
-// }
-
-// renderPeople();
-
 //ROOT API
 const rootapi = "https://swapi.dev/api/";
 
 //DESCRIBE getPeople() FUNCTION
 async function getPeople() {
-    //get query from index.html
+    //GET QUERY FROM index.html
     const peopleInput = document.getElementById("input-people").value;
 
+    //URL FOR PEOPLE SEARCH
     const url = rootapi + "/people/?search=" + peopleInput;
-
-    try {
-        let response = await fetch(url);
-        let data = await res.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
-async function getPeople() {
-    var inputData = document.getElementById("input-people").value;
-    console.log("User entered: " + inputData);
-
-    let url = "https://swapi.dev/api/people/?search=" + inputData;
 
     try {
         let res = await fetch(url);
         let data = await res.json();
 
-        let container = document.querySelector('.container');
-        container.innerHTML = data.count + data.results[0].name;
-        console.log(data)
+        console.log(data);
+
+        const results = data.results; 
+
+        console.log(results)
+        
+        let html = "";
+
+        results.forEach(result => {
+            let htmlSegment = `<div class="result"
+                <h2>${result.name}</h2>
+                </div>`;
+
+            html += htmlSegment;
+        })
+
+        let container = document.querySelector('#swresult');
+        container.innerHTML = html;
+        
     } catch (error) {
         console.log(error);
     }
-  }
-  
+}
+
+//DESCRIBE getFilms() FUNCTION
