94062e889ec6557b60ba77dbacb655be10a4be67
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-04-20 01:58:44 -0500

    centering loader and searchbar

--- js/app.js
+++ js/app.js
@@ -3,27 +3,53 @@
 const rootAPI = "https://swapi.dev/api/";
 
 //DEFINE getAPI FUNCTION
-async function getAPI(url) {
+async function getAPI() {
+
+    let url = rootAPI + "people/1"
     fetch(url).then(res => {
         if(res.status === 200) {
-            let x = document.getElementById("loader")
-            x.style.visibility = "hidden"
+            let x = document.getElementById("loader");
+            x.style.visibility = "hidden";
         }
-        console.log(res.status);
+        console.log(res.json());
     }).catch(err => {
         console.log(err)
     })
 }
 
-getAPI(rootAPI + "films")
+//getAPI(rootAPI + "films")
 
 //DEFINE getSearch FUNCTION
 function getSearch(category) {
-    console.log(category)
+
+    let swsearch = `<form>
+        <label for="swInput">Search ${category}: </label>
+        <input type="text" id="swInput">
+        <button type="button" onclick="getAPI()">Search</button>
+        </form>
+    `
+    let section = document.getElementById("swSearch");
+    section.innerHTML = swsearch
 }
 
+function getSearchValue() {
+
+}
     // fetch(apiURL).then(response => {
     //     console.log(response.status); 
     // }).catch(error => {
     //     console.error(error);
     // });
+
+        //WHEN SOMEONE HITS ENTER IN SEARCH BOX
+        // const input = document.getElementById("swInput");
+        // const swValue = input.value;
+    
+        // const url = rootAPI + category.toLowerCase() + "/?search=" + swValue;
+    
+        // input.addEventListener('keydown', event => {
+        //     if(event.key === "Enter") {
+        //         event.preventDefault();
+        //         getAPI(url);
+        //     }
+        // })
